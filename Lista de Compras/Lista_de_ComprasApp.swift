6b96aca0055c33d841cd471dafb2b750c650a537
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-05-08 19:53:17 -0300

    Initial Commit

---
+++ Lista de Compras/Lista_de_ComprasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lista_de_ComprasApp.swift
+//  Lista de Compras
+//
+//  Created by Boris R on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Lista_de_ComprasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
